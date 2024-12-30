c5893c15526277e8a885e0de0ac4c2ba8a295ab8
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-12-30 12:23:54 +0530

    Cors Configuration added

--- src/main/java/com/uor/eng/config/CorsConfig.java
+++ src/main/java/com/uor/eng/config/CorsConfig.java
@@ -14,25 +14,12 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-
-    // Specify the origins that are allowed to make requests
     configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-
-    // Specify the HTTP methods that are allowed
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-    // Specify the headers that are allowed in requests
     configuration.setAllowedHeaders(List.of("*"));
-
-    // Allow credentials (e.g., cookies, authorization headers)
     configuration.setAllowCredentials(true);
-
-    // Optionally, specify headers that can be exposed to the client
     configuration.setExposedHeaders(List.of("Authorization"));
-
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-    // Apply the CORS configuration to specific URL patterns
     source.registerCorsConfiguration("/api/**", configuration);
 
     return source;
