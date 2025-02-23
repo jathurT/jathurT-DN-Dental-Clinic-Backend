6428eac3f22ae0e9bc5224286d7d130cf19313d6
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2025-02-23 09:03:04 +0530

    Go to production

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Wait for MySQL to be ready
+echo "Waiting for MySQL to be ready..."
 until nc -z -v -w30 mysql 3306
 do
-  echo "Waiting for MySQL to be ready..."
+  echo "Waiting for MySQL..."
   sleep 5
 done
 
