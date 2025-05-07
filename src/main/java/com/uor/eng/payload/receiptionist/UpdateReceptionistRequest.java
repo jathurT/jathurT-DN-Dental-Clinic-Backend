114b13b03c1a8c30c5da9ad4ab7913150aa8ef54
jathurT <ktmjathur2001@gmail.com>
2025-05-07 16:57:14 +0530

    s3 service test cases updated

--- src/main/java/com/uor/eng/payload/receiptionist/UpdateReceptionistRequest.java
+++ src/main/java/com/uor/eng/payload/receiptionist/UpdateReceptionistRequest.java
@@ -4,9 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateReceptionistRequest {
   @NotBlank(message = "Username is mandatory")
   @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
