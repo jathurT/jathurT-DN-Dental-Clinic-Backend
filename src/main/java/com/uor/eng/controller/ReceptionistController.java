912c098a984f6ae2d2db05e59e051769f916b8cb
jathurT <ktmjathur2001@gmail.com>
2025-05-07 21:57:57 +0530

    s3 service test cases updated

--- src/main/java/com/uor/eng/controller/ReceptionistController.java
+++ src/main/java/com/uor/eng/controller/ReceptionistController.java
@@ -5,7 +5,6 @@ import com.uor.eng.payload.receiptionist.ReceptionistResponseDTO;
 import com.uor.eng.payload.receiptionist.UpdateReceptionistRequest;
 import com.uor.eng.service.IReceptionistService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
